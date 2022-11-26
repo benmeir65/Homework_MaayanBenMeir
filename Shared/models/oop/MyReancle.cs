6202a05c0180b7c8f0082fe385f7c41433b2f50c
benmeir65 <107880688+benmeir65@users.noreply.github.com>
2022-11-26 19:14:53 +0200

    Lesson5

---
+++ Shared/models/oop/MyReancle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Shared.models.oop
+{
+    public class MyReancle
+    {
+
+		public string Color { get; set; }
+		public double Length { get; set; }
+		public double Width { get; set; }
+
+		public double R_area()
+		{
+			double area = Length * Width;
+			return area;
+		}
+		public double R_circumference()
+		{
+			double circumference = (Length * 2) + (Width * 2);
+			return circumference;
+		}
+
+
+
+	}
+}
