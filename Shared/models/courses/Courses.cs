e1f5b62275796da400c47923b463e69f1b985d2e
benmeir65 <107880688+benmeir65@users.noreply.github.com>
2022-12-24 17:37:15 +0200

    Lesson7

---
+++ Shared/models/courses/Courses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Shared.models.courses
+{
+       public class Courses
+    {
+        public string CourseName(get; get;)
+        public string TeacherName(get; get;)
+        public double CourseCredit(get; get;)
+       public boll IsExam(get; get;) 
+           
+    }
+}
---
+++ Shared/models/oop/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Shared.models.oop
+{
+    public class Class1
+    {   
+        public string name { get; set; }
+        public int numofbottles { get; set; }
+        public int numofvaccinations { get; set; }
+
+        public int calculate(int amount)
+        {
+
+          double  remain =  ((numofbottles * amount) - numofvaccinations );
+            return remain;
+        }
+
+
+
+
+    }
+}
